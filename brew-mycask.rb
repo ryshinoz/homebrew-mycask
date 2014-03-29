0c467b6098ef8bedbf27565ceb00a44dd1bb1b79
Ryosuke Shinozaki <ryshinoz@gmail.com>
2014-03-30 00:25:42 +0900

    first commit

---
+++ brew-mycask.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewMycask < Formula
+  homepage 'https://github.com/ryshinoz/homebrew-mycask'
+  url 'https://github.com/ryshinoz/homebrew-mycask.git'
+  def install
+  end
+end
