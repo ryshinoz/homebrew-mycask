1027d1d1951ece29194336456840a744e0b4eb02
Ryosuke Shinozaki <ryshinoz@gmail.com>
2014-03-30 00:42:15 +0900

    add iterm2 beta

---
+++ Casks/iterm2-beta.rb
@@ -0,0 +1,7 @@
+class iTerm2 < Cask
+  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20140112.zip'
+  homepage 'http://www.iterm2.com/'
+  version '1.0.0.20140112'
+  sha1 '89020eebda23c1cf0cea89343a50e0a7a415b172'
+  link 'iTerm.app'
+end
