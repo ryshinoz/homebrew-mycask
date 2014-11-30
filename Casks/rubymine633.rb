573da63e5f373604c5efc26832a5af9f517914aa
Ryosuke Shinozaki <ryshinoz@gmail.com>
2014-11-30 11:08:37 +0900

    checksum修正

--- Casks/rubymine633.rb
+++ Casks/rubymine633.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rubymine633' do
   version '6.3.3'
-  sha256 'c79216de02f2564ea60592420342ab9fb5014da7e7c96f92e2856dc49f2090dd`'
+  sha256 'c79216de02f2564ea60592420342ab9fb5014da7e7c96f92e2856dc49f2090dd'
 
   url "http://download-cf.jetbrains.com/ruby/RubyMine-#{version}.dmg"
   homepage 'http://www.jetbrains.com/ruby/'
